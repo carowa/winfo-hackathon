c2f13b72808277b5d2c8a1a13eff06ce73d41db8
wolfj4 <wolfj4@cs.washington.edu>
2018-01-13 15:38:06 -0800

    filter implemented

--- student_view/js/controller.js
+++ student_view/js/controller.js
@@ -3,9 +3,24 @@ var database = firebase.database();
 
 
 function handleSubmit () {
-  //function writeUserData(userId, name, email, imageUrl) {
-  firebase.database().ref('questions').set({
-    question: document.getElementById('validationDefault01').value
-  });
-  //}
+
+  let q = document.getElementById('validationDefault01').value;
+
+  let badword1 = new RegExp('.*damn.*', 'i');
+  let badword2 = new RegExp('.*fuck.*', 'i');
+  let badword3 = new RegExp('.*shit.*', 'i');
+  let badword4 = new RegExp('.*bitch.*', 'i');
+
+  let isBad1 = badword1.test(q);
+  let isBad2 = badword2.test(q);
+  let isBad3 = badword3.test(q);
+  let isBad4 = badword4.test(q);
+
+  if (!isBad1 && !isBad2 && !isBad3 && !isBad4) {
+    //function writeUserData(userId, name, email, imageUrl) {
+    firebase.database().ref('questions').set({
+      question: q
+    });
+    //}
+  }
 }
