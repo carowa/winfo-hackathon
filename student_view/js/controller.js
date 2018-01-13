1208e7f7f47e0ba7a558b3af72cb66a1155f2dcd
michael lee <keymikey54@yahoo.com>
2018-01-13 15:13:36 -0800

    updates

--- student_view/js/controller.js
+++ student_view/js/controller.js
@@ -4,7 +4,7 @@ var database = firebase.database();
 
 function handleSubmit () {
   //function writeUserData(userId, name, email, imageUrl) {
-  firebase.database().ref('person').set({
+  firebase.database().ref('questions').set({
     question: document.getElementById('validationDefault01').value
   });
   //}
