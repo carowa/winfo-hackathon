42fbef8884391a4ff97cc13b502b937ef117c57e
wolfj4 <wolfj4@cs.washington.edu>
2018-01-13 18:40:10 -0800

    added a fake badword for demo

--- student_view/js/controller.js
+++ student_view/js/controller.js
@@ -11,14 +11,16 @@ function handleSubmit () {
   let badword3 = new RegExp('.*shit.*', 'i');
   let badword4 = new RegExp('.*bitch.*', 'i');
   let badword5 = new RegExp('.*ass.*', 'i');
+  let badword6 = new RegExp('.*badword.*', 'i');
 
   let isBad1 = badword1.test(q);
   let isBad2 = badword2.test(q);
   let isBad3 = badword3.test(q);
   let isBad4 = badword4.test(q);
   let isBad5 = badword5.test(q);
+  let isBad6 = badword6.test(q);
 
-  if (!isBad1 && !isBad2 && !isBad3 && !isBad4) {
+  if (!isBad1 && !isBad2 && !isBad3 && !isBad4 && !isBad5 && !isBad6) {
       firebase.database().ref('CSE142/Lec1').push({
         question: document.getElementById('validationDefault01').value
       });
